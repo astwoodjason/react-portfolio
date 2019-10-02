53c69a5a2ba0f433ef300f3d0dce77762a2c5031
astwoodjason <soulsigns123@gmail.com>
2019-10-02 15:41:48 -0600

    I hate pushing

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>playboicarti.</h2>
+            </div>
+        )
+    }
+}
