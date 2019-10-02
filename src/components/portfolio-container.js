89f09efdeaabea10cce832972fa6e94ae19a6d71
astwoodjason <soulsigns123@gmail.com>
2019-10-02 15:45:41 -0600

    I love pushing

--- src/components/portfolio-container.js
+++ src/components/portfolio-container.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
+import PortfolioItem from "./portfolio-item";
+
 export default class PortfolioContainer extends Component {
     render() {
         return (
             <div>
                 <h2>playboicarti.</h2>
+
+                <PortfolioItem />
             </div>
-        )
+        );
     }
 }
